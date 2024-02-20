f7dd13adb4acc7813afa1799dc8fc7d67c34ecdd
ldh0308 <142721325+ldh0308@users.noreply.github.com>
2024-02-20 19:37:51 +0900

    member commit

--- src/test/java/hello/core/member/MemberServiceTest.java
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,2 +1,24 @@
-package hello.core.member;public class MemberServiceTest {
+package hello.core.member;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class MemberServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+
+    @Test
+    @DisplayName("조인테스트")
+    void join() {
+        //given
+        Member member = new Member(1L, "memberA", Grade.VIP);
+
+        //when
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+
+        //then
+        Assertions.assertThat(member).isEqualTo(findMember);
+    }
 }
