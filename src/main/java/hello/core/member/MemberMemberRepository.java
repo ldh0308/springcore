4ce77444885dbe70627dc4c222b32df19a6374d5
ldh0308 <142721325+ldh0308@users.noreply.github.com>
2024-02-21 16:13:08 +0900

    설명 추가

--- src/main/java/hello/core/member/MemberMemberRepository.java
+++ src/main/java/hello/core/member/MemberMemberRepository.java
@@ -5,11 +5,11 @@ import java.util.Map;
 
 public class MemberMemberRepository implements MemberRepository {
 
-    private static Map<Long, Member> store = new HashMap<>();
+    private static Map<Long, Member> store = new HashMap<>(); // store 객체 생성
 
     @Override
     public void save(Member member) {
-        store.put(member.getId(), member);
+        store.put(member.getId(), member); // member.getId 는 Long <key>, member <value>
     }
 
     @Override
